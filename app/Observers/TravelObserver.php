5fd653c06713e914940d5f35d1284263ed167120
Tomasz Stein <7057676u@zstzbaszynek.pl>
2023-10-06 14:51:46 +0200

    check

---
+++ app/Observers/TravelObserver.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Travel;
+
+class TravelObserver
+{
+    public function creating(Travel $travel): void
+    {
+        $travel->slug =str($travel->name)->slug();
+    }
+
+}
